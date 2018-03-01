8da1279ad3392955ad2818bf1ef21e365bf71681
Mohamed Oumoumad <oumoumad@gmail.com>
2018-03-01 14:01:38 +0100

    updated docs

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+	//on documentation click
 	$("#documentation").on("click",function(){
 		$(".page-two").css({
 			"display": "block"
@@ -13,15 +14,15 @@ $(function(){
 				})
 			}
 		});
-
 		$(".page-one").velocity({
 			left:["-100%","0"]
 		});
-
 		$("footer ul").css({
 			"justify-content": "left"
 		})
 	})
+
+	//on logo click
 	$(".top-logo").on("click",function(){
 		$(".page-one").velocity({
 			left:["0%","-100"]
@@ -37,7 +38,6 @@ $(function(){
 				})
 			}
 		})
-
 		$("footer ul").css({
 			"justify-content": "center"
 		})
@@ -49,6 +49,47 @@ $(function(){
 	if(anchor == "documentation"){
 		$("#documentation").trigger("click")
 	}
+
+	//on about click
+	$("#about").on("click",function(){
+		$(".overlay").css({display:"block"})
+		$(".about").velocity({
+			top:["0%","100%"]
+		})
+		$(".overlay").velocity({
+			opacity:["1","0"]
+		})
+	})
+
+	//on about click
+	$("#about").on("click",function(){
+		$(".overlay").css({display:"block"})
+		$(".about").velocity({
+			top:["0%","100%"]
+		})
+		$(".overlay").velocity({
+			opacity:["1","0"]
+		})
+	})
 	
+	//on overlay click
+	$(".overlay").on("click",function(){
+		$(".overlay").velocity({
+			opacity:["0","1"]
+		},{
+    /* Log all the animated divs. */
+    	complete: function(elements) { $(".overlay").css({display:"none"})}
+		})
+		$(".about").velocity({
+			top:["100%","0%"]
+		})
+	})
 
 })
+
+
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
